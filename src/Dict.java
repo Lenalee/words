cf55595fd02a9c694bd4eb6e4cd88bf645542e55
Lenalee <zajchig@gmail.com>
2015-05-17 09:38:29 +0200

    Update Dict.java

--- src/Dict.java
+++ src/Dict.java
@@ -14,8 +14,16 @@ import java.util.ArrayList;
 public class Dict {
     ArrayList<String> list;
    
+    public class Dict {
+    
+    FreqMapPos pos;
+    
+    ArrayList<String> list;
+   
     public Dict(){
         list = new ArrayList<>();
+        pos = new FreqMapPos();
+        
     }
     
     void add(final String s){
@@ -25,20 +33,68 @@ public class Dict {
         return k.split("");
     }
     // _ matches for any letter
-    Boolean matchMask(final String mask, final String word) {
-        if (mask.length() != word.length()) 
+    static Boolean matchMask(final String mask, final String word) {
+        if (mask.length() != word.length()) {
             return false;
-        Integer pos = 0;
-        for(String s: split(mask)) {
-           pos = pos + 1;
+        }  
         
+        Integer pos = 0;
+        String[] maskk = mask.split("");
+        
+        for(String s: word.split("")) {
+            if(maskk[pos].equals("_") ||  s.equals(maskk[pos])) {
+                pos = pos +1;
+              } else {
+                return false;
+            }   
         }
         return true;
     }
+     
     
     Dict reduceDict(String mask) {
         Dict newDict = new Dict();
-        
+        for(String s : list){
+            if(matchMask(mask, s)) {
+                newDict.add(s);
+            }
+        }
+        newDict.frekvence();
         return newDict;
     }
+    
+    void frekvence() {
+        for(String word : list) {
+            int i = 0;   
+            for(String cha : word.split(word)){
+                FreqMap pos_freq = pos.getMap(i);
+                pos_freq.count_char(cha);
+                i = i + 1;
+            }
+        }
+        for(FreqMap f : pos.getValues()) {
+            f.calc_frekvence();
+        }
+    }
+    Pary maxFreq(){
+        double max = 0;
+        Integer maxPos = 0;
+        String maxch = "";
+        for(Integer i : pos.getKeys()){
+            FreqMap f = pos.posMap.get(i);
+            for(String s: f.getKeys()){
+                double freq = f.getFreq(s);
+                if(freq > max){
+                   max = freq;
+                   maxch = s;
+                   maxPos = i;
+                }
+                
+            }
+        
+        }
+        return new Pary(maxPos,maxch);
+    }
 }
+
+   
